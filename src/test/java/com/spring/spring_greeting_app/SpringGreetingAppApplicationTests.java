feac819ccd7027e90337c7e2a7b3101090ae8c46
ayush <ayushmaurya2050@gmail.com>
2025-02-22 11:22:40 +0530

    [AYUSH KUMAR MAURYA] Add: setup project

---
+++ src/test/java/com/spring/spring_greeting_app/SpringGreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_greeting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
